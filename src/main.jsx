b4e40152b87329d7ee7fce53edc0ba9d62250063
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-09-27 14:56:28 -0300

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './global.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
